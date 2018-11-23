4222f713d7eaa9bc1551b7eb94bba9120b21bc89
Marcin Radzio <enevyn@gmail.com>
2018-11-23 01:50:18 +0100

    hidden_size testing preparation

--- train.py
+++ train.py
@@ -5,20 +5,18 @@ trainingDataPaths = loadListFromFile("Processed data/training_set.txt")
 validationDataPaths = loadListFromFile("Processed data/validation_set.txt")
 
 input_size = pd.read_csv(trainingDataPaths[0]).shape[1]
-hidden_size = 200
+hidden_size = int(sys.argv[1])
 stacks = 1
 output_size = 2
 model = RNN(input_size, hidden_size, stacks, output_size).double()
 
-epochs = 10
+epochs = 200
 batch_size = 100
 seq_size = 50
 loss_fn = torch.nn.CrossEntropyLoss()
-learning_rate = float(sys.argv[1])
+learning_rate = 0.01 #best 0.01 (or 1/batch_size)
 optimizer = torch.optim.Adadelta(model.parameters(), lr=learning_rate)
 
-target_syncope = torch.LongTensor([0]).repeat(batch_size*seq_size)
-target_nosyncope = torch.LongTensor([1]).repeat(batch_size*seq_size)
 batcher = Batcher(trainingDataPaths, seq_size, batch_size)
 
 #print("EPOCH\t\tTRAINING LOSS\t\t\tVALIDATION LOSS")
@@ -40,7 +38,7 @@ for epoch in range(epochs):
     
     optimizer.step()
     print(loss.detach().numpy(), flush=True)
-  torch.save(model, 'checkpoints/'+str(learning_rate)+".model")
+  torch.save(model, 'checkpoints/'+str(hidden_size)+".model")
   batcher.nextEpoch()
   
   
