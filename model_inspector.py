f026d31efaf264603715f62c98319a56cd8564f9
Marcin Radzio <enevyn@gmail.com>
2019-06-09 09:51:31 +0200

    final results

--- model_inspector.py
+++ model_inspector.py
@@ -9,7 +9,7 @@ print("Hidden size:\t" + str(model.hidden_size), flush=True)
 print("Depth:\t\t" + str(model.stacks), flush=True)
 print("Output size:\t" + str(model.output_size), flush=True)
 try:
-  #Old RNN definition did not contain bidirectional field
   print("Bidirectional:\t" + str(model.bidirectional), flush=True)
 except:
-  print("Bidirectional:\tFalse", flush=True)
+  #Old RNN definition did not contain bidirectional field
+  print("Bidirectional:\tUnknown", flush=True)
